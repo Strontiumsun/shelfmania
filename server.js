c7db6f3a0aec4951e005348bfa841eb22bbd383d
Strontiumsun <izzyhall25@gmail.com>
2019-06-29 15:41:21 -0500

    Created server file

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var mongoose = require("mongoose");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to the Mongo DB
+mongoose.connect('mongodb://localhost/shelfmania', { useNewUrlParser: true })
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+;
+
+app.listen(PORT, function () {
+    console.log('App listening on PORT:' + PORT);
+});
